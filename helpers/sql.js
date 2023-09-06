5bdcc78f82cfbeac36e3bda4a91a350358538351
JarekDSmall <129123790+JarekDSmall@users.noreply.github.com>
2023-09-06 16:13:38 -0400

    Refactor sql.js helper function and update documentation
    Renamed function to `generateSqlForUpdate` for clarity.
    - Updated error handling to use `DataError` class.
    - Enhanced JSDoc comments to provide a clearer understanding of the function's purpose and usage.

--- helpers/sql.js
+++ helpers/sql.js
@@ -1,20 +1,36 @@
-const { BadRequestError } = require("../expressError");
+const { DataError } = require("../customErrors");
 
-// THIS NEEDS SOME GREAT DOCUMENTATION.
+/**
+ * Utility function to assist in creating SQL update queries.
+ *
+ * This function can be utilized to generate the SET clause for an SQL UPDATE
+ * command.
+ *
+ * @param dataToUpdate {Object} {attribute1: newVal, attribute2: newVal, ...}
+ * @param jsToSql {Object} translates js-style data attributes to database column identifiers,
+ *   e.g., { firstName: "first_name", age: "age" }
+ *
+ * @returns {Object} {sqlSetCols, dataToUpdate}
+ *
+ * @example {firstName: 'Aria', age: 30} =>
+ *   { sqlSetCols: '"first_name"=$1, "age"=$2',
+ *     values: ['Aria', 30] }
+ */
 
-function sqlForPartialUpdate(dataToUpdate, jsToSql) {
+function generateSqlForUpdate(dataToUpdate, jsToSql) {
   const keys = Object.keys(dataToUpdate);
-  if (keys.length === 0) throw new BadRequestError("No data");
+  if (keys.length === 0) throw new DataError("Missing data");
 
-  // {firstName: 'Aliya', age: 32} => ['"first_name"=$1', '"age"=$2']
+  // {firstName: 'Aria', age: 30} => ['"first_name"=$1', '"age"=$2']
   const cols = keys.map((colName, idx) =>
       `"${jsToSql[colName] || colName}"=$${idx + 1}`,
   );
 
   return {
-    setCols: cols.join(", "),
+    sqlSetCols: cols.join(", "),
     values: Object.values(dataToUpdate),
   };
 }
 
-module.exports = { sqlForPartialUpdate };
+module.exports = { generateSqlForUpdate };
+
